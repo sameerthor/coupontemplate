b1f85762624b79a3268ef0bd4e7c177596bf8c87
shivam4srtech <shivam.srtech@gmail.com>
2025-01-07 11:27:52 +0530

    meta

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,6 +19,7 @@ export default function App({ Component, pageProps }) {
   return (
     <main className={inter.className}>
           <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
             <style>{dom.css()}</style>
             <script src="/custom.js" defer></script>
           </Head>
