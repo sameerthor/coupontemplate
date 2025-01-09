6a023472717d9712420a2ca798587ef9293ca34c
shivam4srtech <shivam.srtech@gmail.com>
2025-01-09 17:22:00 +0530

    terms

--- src/components/footer.js
+++ src/components/footer.js
@@ -160,9 +160,6 @@ export default function Footer() {
                                 </svg>{" "}
                                 Coupons
                             </Link>
-                        </div>
-                        <div className="col-lg-2 col-md-4 col-sm-12 mb-3 footer-links">
-                            <h3>QUICK LINKS</h3>
                             <Link href="/category">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -183,6 +180,9 @@ export default function Footer() {
                                 </svg>{" "}
                                 Categories
                             </Link>
+                        </div>
+                        <div className="col-lg-2 col-md-4 col-sm-12 mb-3 footer-links">
+                            <h3>Legal</h3>
                             <Link href="/contact">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -243,6 +243,26 @@ export default function Footer() {
                                 </svg>{" "}
                                 Cookie Policy
                             </Link>
+                            <Link href="/terms-of-use">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width={16}
+                                    height={16}
+                                    fill="currentColor"
+                                    className="bi bi-chevron-double-right"
+                                    viewBox="0 0 16 16"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708"
+                                    />
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708"
+                                    />
+                                </svg>{" "}
+                               Terms of use
+                            </Link>
                         </div>
                         <div className="col-lg-3 col-md-4 col-sm-12 mb-3 footer-links">
                             <h3>Stay Updated</h3>
@@ -267,7 +287,7 @@ export default function Footer() {
                                 Amazon.com, Inc. or its affiliates.
                             </p>
                             <p className="lovtxt">
-                                Made with love ❤️ from UNited Kingdom.
+                                Made with love ❤️ from United Kingdom.
                             </p>
                             <p className="lovtxt">
                                 Copyright © 2024 SuperCosts.com All Right Reserved
